bc8bd664a2b2ed01b6093afb8bbb5b4c33f1fd62
Pradip More <pradipmorepm15@gmail.com>
2022-07-27 22:43:02 +0530

    done

--- sumTillN.cpp
+++ sumTillN.cpp
@@ -1,11 +1,22 @@
 #include<iostream>
 using namespace std;
+int sumfun(int n){
+    if(n==0){
+        return 0;
+    }
+    return n + sumfun(n-1);
+}
 int main(){
     int n,sum=0;
     cin>>n;
+    /*
     for(int i=1;i<=n;i++){
         sum+=i;
     }
     cout<<sum<<endl;
+    */
+   /*2nd method recursion*/
+   cout<<sumfun(n)<<endl;
+
     return 0;
 }
