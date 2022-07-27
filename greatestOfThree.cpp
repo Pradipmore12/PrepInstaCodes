3b5f6786d6ccef90741b05de381fa5d1bc1c338d
Pradip More <pradipmorepm15@gmail.com>
2022-07-27 22:54:51 +0530

    greatAthree

---
+++ greatestOfThree.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a>b && a>c){
+        cout<<"greater number is: "<<a<<endl;
+    }
+    else if(b>a && b>c){
+        cout<<"greater number is: "<<b<<endl;
+    }
+    else{
+        cout<<"greater number is: "<<c<<endl;
+    }
+    return 0;
+}
