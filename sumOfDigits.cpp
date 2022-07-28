4845c2dca8d2c90e1929ebd2aab09774d1d8aaa6
Pradipmore12 <en19320803@git-india.edu.in>
2022-07-28 21:11:32 +0530

    sumOfDigit

---
+++ sumOfDigits.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cin>>num;
+    int sum=0;
+    while (num>0)
+    {
+        int digit=num%10;
+        sum+=digit;
+        num/=10;
+        
+    }
+    cout<<sum<<endl;
+    
+    return 0;
+}
