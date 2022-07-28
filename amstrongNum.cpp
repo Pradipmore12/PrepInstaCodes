a3fca074e8474c4d9e8759102cffd6fe25b7ecb2
Pradipmore12 <en19320803@git-india.edu.in>
2022-07-28 22:05:14 +0530

    formatDoc

--- amstrongNum.cpp
+++ amstrongNum.cpp
@@ -1,18 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int num;
-    cin>>num;
-    int originalNum=num;
-    int sum=0;
-    while(num>0){
-        int digit=num%10;
-        sum = sum + digit*digit*digit;
-        num/=10;
+    cin >> num;
+    int originalNum = num;
+    int sum = 0;
+    while (num > 0)
+    {
+        int digit = num % 10;
+        sum = sum + digit * digit * digit;
+        num /= 10;
     }
-    if(sum==originalNum)
-        cout<<"amstrong"<<endl;
+    if (sum == originalNum)
+        cout << "amstrong" << endl;
     else
-        cout<<"not amstrong "<<endl;
+        cout << "not amstrong " << endl;
     return 0;
 }
--- pallindromeNum.cpp
+++ pallindromeNum.cpp
@@ -1,21 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
 
-    int num ,reverse=0;
-    cin>>num;
-    int originalNum=num;
-    while(num>0){
-        int digit=num%10;
-        reverse=reverse*10+digit;
-        num/=10;
+    int num, reverse = 0;
+    cin >> num;
+    int originalNum = num;
+    while (num > 0)
+    {
+        int digit = num % 10;
+        reverse = reverse * 10 + digit;
+        num /= 10;
     }
-    if(reverse==originalNum)
-        cout<<"pallindrome"<<endl;
+    if (reverse == originalNum)
+        cout << "pallindrome" << endl;
     else
-        cout<<"not pallindrome"<<endl;
-
-
+        cout << "not pallindrome" << endl;
 
     return 0;
 }
