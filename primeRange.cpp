eab2e8f379e644849d2ac0a4e7133837e8672928
Pradipmore12 <en19320803@git-india.edu.in>
2022-07-28 20:58:36 +0530

    primeRange

---
+++ primeRange.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a ,b;
+    cin>>a>>b;
+    int j;
+    bool prime;
+
+    for(int i=a;i<=b;i++){
+        prime=true;
+        if(i==1)
+            prime= false;
+        for(j=2;j<i;j++){
+            if(i%j==0){
+                prime=false;
+                break;
+
+            }
+        }
+        if(prime)
+            cout<<i<<" ";
+    }
+    return 0;
+}
