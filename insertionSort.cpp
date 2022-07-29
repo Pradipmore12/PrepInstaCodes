3f95d1750824a75589ca07f55fc5e053ef3537a8
Pradipmore12 <en19320803@git-india.edu.in>
2022-07-29 20:29:29 +0530

    formatDoc

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -1,28 +1,33 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void insertionSort(int arr[],int n){
-    for(int i=1;i<n;i++){
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
         int current = arr[i];
-        int j=i-1;
-        while(arr[j]>current && j>=0){
-            arr[j+1]=arr[j];
+        int j = i - 1;
+        while (arr[j] > current && j >= 0)
+        {
+            arr[j + 1] = arr[j];
             j--;
         }
-        arr[j+1]=current;
-
+        arr[j + 1] = current;
     }
     return;
 }
-int main(){
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    insertionSort(arr,n);
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+    insertionSort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
 
     return 0;
