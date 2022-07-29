aeabe935e714f914501e244c1018deac20a27a75
Pradipmore12 <en19320803@git-india.edu.in>
2022-07-29 19:58:59 +0530

    formatDoc

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,27 +1,34 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void bubbleSort(int arr[],int n){
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i;j++){
-            if(arr[j]>arr[j+1]){
-                int temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
     return;
 }
-int main(){
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    bubbleSort(arr,n);
-     for(int i=0;i<n;i++){
-        cout<<arr[i];
+    bubbleSort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i];
     }
     return 0;
 }
