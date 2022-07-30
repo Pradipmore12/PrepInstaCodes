e8076c36a945cb03dc40e591030bb896459869bc
Pradipmore12 <en19320803@git-india.edu.in>
2022-07-30 22:19:07 +0530

    amstrongInRaneg

---
+++ amstrongGivenRange.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool amstrong(int n){
+    int originalNum=n;
+    int sum=0;
+    while(n>0){
+        int digit = n%10;
+        sum+=digit*digit*digit;
+        n/=10;
+
+    }
+    if(sum==originalNum){
+        return true;
+    }
+        
+    else 
+        return false;
+}
+int main(){
+
+    int a,b;
+    cin>>a>>b;
+    for(int i=a;i<=b;i++){
+        if(amstrong(i))
+            cout<<i<<" ";
+    }
+    return 0;
+}
