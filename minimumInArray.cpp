eae1781e3fde6a12afb34bd0976a736a2590b401
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-01 22:13:27 +0530

    maxinArray

---
+++ minimumInArray.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int getMax(int num[] , int size)
+{
+    int max = INT_MIN;
+    for(int i = 0 ; i < size ; i++)
+    {
+        if(num[i]>max)
+            max = num[i];
+    }
+    return max;
+}
+int main(){
+    int size;
+    cin >> size;
+    int num[100];
+    for (int i = 0 ; i < size ; i++ )
+    {
+        cin >> num[i];
+    }
+    cout<<"max element is : "<<getMax(num,size)<<endl;
+    return 0;
+}
