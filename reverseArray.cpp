e0817532ba4f4ed55d49595062d499fea7b0a2aa
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-01 23:01:03 +0530

    reverseArray

--- reverseArray.cpp
+++ reverseArray.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 void reverse(int arr[], int size)
 {
-    for (int i = size-1; i >= 0; i--)
+    for (int i = size - 1; i >= 0; i--)
     {
         cout << arr[i] << " ";
     }
