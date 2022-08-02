fd50157836f94564bc980a71f2d692998b10b3ba
Pradipmore12 <en19320803@git-india.edu.in>
2022-08-02 19:49:41 +0530

    alternateReverse

---
+++ alternateReverse.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int size;
+    cin>>size;
+    int arr[100];
+    for ( int i=0; i<size;i++)
+    {
+        cin >> arr[i];
+    }
+
+    // main code begins here
+    int start = 0;
+    int end = 1;
+    while(start <= size-1 && end <= size-1)
+    {
+        int temp= arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start+=2;
+        end+=2;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
